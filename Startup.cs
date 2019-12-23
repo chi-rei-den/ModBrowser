0292e431b4f8d726d50c27e455403a9e1d770d96
SGKoishi <i@sgkoi.dev>
2019-12-23 10:18:36 -0800

    Fix typo, disable SSL (use nginx/apache)

--- Startup.cs
+++ Startup.cs
@@ -46,10 +46,10 @@ namespace ModBrowser
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
