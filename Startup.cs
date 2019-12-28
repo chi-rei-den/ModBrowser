4f6d46d67c31991239de31f1d36d99f5a95e4dc7
SGKoishi <i@sgkoi.dev>
2019-12-27 19:42:17 -0800

    Add timestamp to console logging

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using ModBrowser.Data;
 using ModBrowser.Models;
 using ModBrowser.Services;
@@ -27,6 +28,7 @@ namespace ModBrowser
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(opt => opt.AddConsole(c => c.TimestampFormat = "[HH:mm:ss] "));
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=.\\sqlite.db"));
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
