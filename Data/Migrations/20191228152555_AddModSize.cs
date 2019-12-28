a88f9c8982cb862b62351905d93b1f18ef3c6cf9
SGKoishi <i@sgkoi.dev>
2019-12-28 07:37:12 -0800

    Add mos size

---
+++ Data/Migrations/20191228152555_AddModSize.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ModBrowser.Data.Migrations
+{
+    public partial class AddModSize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Size",
+                table: "Mod",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Mod");
+        }
+    }
+}
