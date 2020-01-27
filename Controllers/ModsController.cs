b603fbd9ecfb8cd541a8fb313bb320896e51227c
鱼鱼 <axeel@qq.com>
2020-01-27 15:30:57 +0800

    u

--- Controllers/ModsController.cs
+++ Controllers/ModsController.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
--- Services/SyncService.cs
+++ Services/SyncService.cs
@@ -24,7 +24,7 @@ namespace Chireiden.ModBrowser.Services
         private readonly ILogger<SyncService> _logger;
         internal static HttpClient Http = new HttpClient() { Timeout = new TimeSpan(0, 5, 0) };
         internal static readonly TimeSpan Interval = TimeSpan.FromMinutes(30);
-        internal static Version tModLoaderVersion = new Version("0.11.6.1");
+        internal static Version tModLoaderVersion = new Version("0.11.6.2");
         internal static ConcurrentQueue<string> UpdateRequested = new ConcurrentQueue<string>();
 
         public SyncService(IServiceScopeFactory scopeFactory, ILogger<SyncService> logger)
@@ -41,187 +41,213 @@ namespace Chireiden.ModBrowser.Services
                 Directory.CreateDirectory(modsFolder);
             }
 
-            await Task.Delay(TimeSpan.FromSeconds(15));
+            await Task.Delay(TimeSpan.FromSeconds(1));
 
             while (true)
             {
-                var requested = new HashSet<string>();
-                while (UpdateRequested.TryDequeue(out var result))
-                {
-                    requested.Add(result);
-                }
-
-                string str;
                 try
                 {
-                    str = await Http.PostAsync("http://javid.ddns.net/tModLoader/listmods.php", new FormUrlEncodedContent(new Dictionary<string, string>
+                    var requested = new HashSet<string>();
+                    while (UpdateRequested.TryDequeue(out var result))
                     {
-                        ["modloaderversion"] = tModLoaderVersion.ToString(),
-                        ["platform"] = "w",
-                        ["netversion"] = "4.0",
-                    })).Result.Content.ReadAsStringAsync();
-                    this._logger.LogInformation("Start Sync");
-                }
-                catch
-                {
-                    this._logger.LogInformation("POST listmods.php fail, retry");
-                    await Task.Delay(Interval);
-                    continue;
-                }
-
-                JObject json;
-                try
-                {
-                    json = (JObject)JsonConvert.DeserializeObject(str);
-                }
-                catch
-                {
-                    this._logger.LogInformation("Json Deserialize exception, retry");
-                    await Task.Delay(Interval);
-                    continue;
-                }
-
-                if (json.ContainsKey("update"))
-                {
-                    tModLoaderVersion = new Version(json["update"].ToString());
-                    this._logger.LogInformation($"Update to {json["update"]}");
-                    continue;
-                }
-
-                if (!json.ContainsKey("modlist_compressed"))
-                {
-                    this._logger.LogInformation("modlist_compressed not found, retry");
-                    await Task.Delay(Interval);
-                    continue;
-                }
-
-                string list;
-                using (var ms = new MemoryStream(Convert.FromBase64String(json["modlist_compressed"].ToString())))
-                {
-                    using var stream = new GZipStream(ms, CompressionMode.Decompress);
-                    using var sr = new StreamReader(stream);
-                    list = sr.ReadToEnd();
-                }
-
-                var modlist = JsonConvert.DeserializeObject<List<Mod>>(list);
-                this._logger.LogInformation($"Unpacked Mod list ({modlist.Count})");
-
-                // Use the version from listmods.php.
-                var versions = modlist.Where(i => i.ModLoaderVersion?.Length > 12).Select(i => new Version(i.ModLoaderVersion.Substring(12))).Max() ?? tModLoaderVersion;
-                var platforms = new List<string>
-                {
-                    $"ExampleMod.v{versions}.zip",
-                    "ModCompile_FNA.zip",
-                    "ModCompile_XNA.zip",
-                    $"tModLoader.Windows.v{versions}.zip",
-                    $"tModLoader.Linux.v{versions}.tar.gz",
-                    $"tModLoader.Mac.v{versions}.zip"
-                };
-
-                if (versions > tModLoaderVersion || platforms.Any(p => !File.Exists(Path.Combine(modsFolder, p))))
-                {
-                    this._logger.LogInformation($"Update tModLoader {versions}");
-                    foreach (var platform in platforms)
-                    {
-                        var downloadURL = $"https://github.com/tModLoader/tModLoader/releases/download/v{versions}/{platform}";
-                        this._logger.LogInformation($"Download {platform} from {downloadURL}");
-                        var compressed = Http.GetByteArrayAsync(downloadURL).Result;
-                        File.WriteAllBytes(Path.Combine(modsFolder, platform), compressed);
+                        requested.Add(result);
                     }
 
-                    tModLoaderVersion = versions;
-                    continue;
-                }
+                    string str;
+                    try
+                    {
+                        str = await Http.PostAsync("http://javid.ddns.net/tModLoader/listmods.php",
+                            new FormUrlEncodedContent(new Dictionary<string, string>
+                            {
+                                ["modloaderversion"] = tModLoaderVersion.ToString(),
+                                ["platform"] = "w",
+                                ["netversion"] = "4.0",
+                            })).Result.Content.ReadAsStringAsync();
+                        this._logger.LogInformation("Start Sync");
+                    }
+                    catch(Exception e)
+                    {
+                        this._logger.LogInformation($"POST listmods.php fail, {e}");
+                        await Task.Delay(Interval);
+                        continue;
+                    }
 
-                var descriptions = Http.GetStringAsync("http://javid.ddns.net/tModLoader/tools/querymodnamehomepagedescription.php").Result;
-                var desclist = JsonConvert.DeserializeObject<List<Mod>>(descriptions).ToDictionary(i => i.Name);
-                foreach (var item in modlist)
-                {
-                    item.Homepage = !string.IsNullOrWhiteSpace(desclist[item.Name].Homepage) ? desclist[item.Name].Homepage : null;
-                    item.Description = !string.IsNullOrWhiteSpace(desclist[item.Name].Description) ? desclist[item.Name].Description : null;
-                }
+                    JObject json;
+                    try
+                    {
+                        json = (JObject) JsonConvert.DeserializeObject(str);
+                    }
+                    catch
+                    {
+                        this._logger.LogInformation("Json Deserialize exception, retry");
+                        await Task.Delay(Interval);
+                        continue;
+                    }
+
+                    if (json.ContainsKey("update"))
+                    {
+                        tModLoaderVersion = new Version(json["update"].ToString());
+                        this._logger.LogInformation($"Update to {json["update"]}");
+                        continue;
+                    }
+
+                    if (!json.ContainsKey("modlist_compressed"))
+                    {
+                        this._logger.LogInformation("modlist_compressed not found, retry");
+                        await Task.Delay(Interval);
+                        continue;
+                    }
+
+                    string list;
+                    using (var ms = new MemoryStream(Convert.FromBase64String(json["modlist_compressed"].ToString())))
+                    {
+                        using var stream = new GZipStream(ms, CompressionMode.Decompress);
+                        using var sr = new StreamReader(stream);
+                        list = sr.ReadToEnd();
+                    }
+
+                    var modlist = JsonConvert.DeserializeObject<List<Mod>>(list);
+                    this._logger.LogInformation($"Unpacked Mod list ({modlist.Count})");
+
+                    // Use the version from listmods.php.
+                    var versions =
+                        modlist.Where(i => i.ModLoaderVersion?.Length > 12)
+                            .Select(i => new Version(i.ModLoaderVersion.Substring(12))).Max() ?? tModLoaderVersion;
+                    var platforms = new List<string>
+                    {
+                        $"ExampleMod.v{versions}.zip",
+                        "ModCompile_FNA.zip",
+                        "ModCompile_XNA.zip",
+                        $"tModLoader.Windows.v{versions}.zip",
+                        $"tModLoader.Linux.v{versions}.tar.gz",
+                        $"tModLoader.Mac.v{versions}.zip"
+                    };
+
+                    if (versions > tModLoaderVersion || platforms.Any(p => !File.Exists(Path.Combine(modsFolder, p))))
+                    {
+                        this._logger.LogInformation($"Update tModLoader {versions}");
+                        foreach (var platform in platforms)
+                        {
+                            var downloadURL =
+                                $"https://github.com/tModLoader/tModLoader/releases/download/v{versions}/{platform}";
+                            this._logger.LogInformation($"Download {platform} from {downloadURL}");
+                            var compressed = Http.GetByteArrayAsync(downloadURL).Result;
+                            File.WriteAllBytes(Path.Combine(modsFolder, platform), compressed);
+                        }
+
+                        tModLoaderVersion = versions;
+                        continue;
+                    }
+
+                    var descriptions = Http
+                        .GetStringAsync("http://javid.ddns.net/tModLoader/tools/querymodnamehomepagedescription.php")
+                        .Result;
+                    var desclist = JsonConvert.DeserializeObject<List<Mod>>(descriptions).ToDictionary(i => i.Name);
+                    foreach (var item in modlist)
+                    {
+                        try
+                        {
+                            item.Homepage = !string.IsNullOrWhiteSpace(desclist[item.Name].Homepage)
+                                ? desclist[item.Name].Homepage
+                                : null;
+                            item.Description = !string.IsNullOrWhiteSpace(desclist[item.Name].Description)
+                                ? desclist[item.Name].Description
+                                : null;
+                        }
+                        catch
+                        {
+                            // ignored
+                        }
+                    }
 #if PAR
                 Parallel.ForEach(modlist, new ParallelOptions
                 {
                     MaxDegreeOfParallelism = 2
                 }, async item =>
 #else
-                foreach (var item in modlist)
+                    foreach (var item in modlist)
 #endif
-                {
-                    try
                     {
-                        using var scope = this.scopeFactory.CreateScope();
-                        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                        var found = db.Mod.Find(item.Name);
-                        item.ModLoaderVersion ??= found?.ModLoaderVersion;
+                        try
+                        {
+                            using var scope = this.scopeFactory.CreateScope();
+                            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                            var found = db.Mod.Find(item.Name);
+                            item.ModLoaderVersion ??= found?.ModLoaderVersion;
 
-                        var mayNeedIcon = !File.Exists(item.IconPath());
-                        var updated = false;
-                        if (found == null)
-                        {
-                            updated = true;
-                        }
-                        else if (requested.Contains(item.Name))
-                        {
-                            updated = true;
-                            this._logger.LogInformation($"Mod {item.DisplayName} ({item.Name}) requested");
-                        }
-                        else if (found.UpdateTimeStamp != item.UpdateTimeStamp)
-                        {
-                            // Authors may change the version as they want, so we don't compare the version
-                            updated = true;
-                            this._logger.LogInformation($"Mod {item.DisplayName} ({item.Name}) {found?.Version} => {item.Version}, {found?.UpdateTimeStamp} => {item.UpdateTimeStamp}");
-                        }
-
-                        if (updated)
-                        {
-                            mayNeedIcon = true;
-                            var result = await Http.GetByteArrayAsync($"http://javid.ddns.net/tModLoader/download.php?Down=mods/{item.Name}.tmod");
-                            File.WriteAllBytes(item.FilePath(), result);
-                            File.SetLastWriteTimeUtc(item.FilePath(), item.GetUpdateTimestamp());
-                            if (!item.ExtractInfo(result, true))
+                            var mayNeedIcon = !File.Exists(item.IconPath());
+                            var updated = false;
+                            if (found == null)
                             {
-                                this._logger.LogInformation($"Unable to extract from {item.Name}");
+                                updated = true;
                             }
-                        }
+                            else if (requested.Contains(item.Name))
+                            {
+                                updated = true;
+                                this._logger.LogInformation($"Mod {item.DisplayName} ({item.Name}) requested");
+                            }
+                            else if (found.UpdateTimeStamp != item.UpdateTimeStamp)
+                            {
+                                // Authors may change the version as they want, so we don't compare the version
+                                updated = true;
+                                this._logger.LogInformation(
+                                    $"Mod {item.DisplayName} ({item.Name}) {found?.Version} => {item.Version}, {found?.UpdateTimeStamp} => {item.UpdateTimeStamp}");
+                            }
 
-                        if (item.Size == 0)
-                        {
-                            item.Size = (int)new FileInfo(item.FilePath()).Length;
-                        }
+                            if (updated)
+                            {
+                                mayNeedIcon = true;
+                                var result = await Http.GetByteArrayAsync(
+                                    $"http://javid.ddns.net/tModLoader/download.php?Down=mods/{item.Name}.tmod");
+                                File.WriteAllBytes(item.FilePath(), result);
+                                File.SetLastWriteTimeUtc(item.FilePath(), item.GetUpdateTimestamp());
+                                if (!item.ExtractInfo(result, true))
+                                {
+                                    this._logger.LogInformation($"Unable to extract from {item.Name}");
+                                }
+                            }
 
-                        if (mayNeedIcon && !string.IsNullOrWhiteSpace(item.IconURL))
-                        {
-                            var result = await Http.GetByteArrayAsync(item.IconURL);
-                            File.WriteAllBytes(item.IconPath(), result);
-                            File.SetLastWriteTimeUtc(item.IconPath(), item.GetUpdateTimestamp());
-                        }
+                            if (item.Size == 0)
+                            {
+                                item.Size = (int) new FileInfo(item.FilePath()).Length;
+                            }
 
-                        if (found == null)
-                        {
-                            this._logger.LogInformation($"Mod {item.DisplayName} ({item.Name}) created.");
-                            db.Mod.Add(item);
-                        }
-                        else
-                        {
-                            db.Entry(found).CurrentValues.SetValues(item);
-                            db.Mod.Update(found);
-                        }
+                            if (mayNeedIcon && !string.IsNullOrWhiteSpace(item.IconURL))
+                            {
+                                var result = await Http.GetByteArrayAsync(item.IconURL);
+                                File.WriteAllBytes(item.IconPath(), result);
+                                File.SetLastWriteTimeUtc(item.IconPath(), item.GetUpdateTimestamp());
+                            }
 
-                        db.SaveChanges();
+                            if (found == null)
+                            {
+                                this._logger.LogInformation($"Mod {item.DisplayName} ({item.Name}) created.");
+                                db.Mod.Add(item);
+                            }
+                            else
+                            {
+                                db.Entry(found).CurrentValues.SetValues(item);
+                                db.Mod.Update(found);
+                            }
+
+                            db.SaveChanges();
+                        }
+                        catch (Exception e)
+                        {
+                            this._logger.LogError($"Mod Update {item.DisplayName} ({item.Name}) : " + e.ToString());
+                        }
                     }
-                    catch (Exception e)
-                    {
-                        this._logger.LogError($"Mod Update {item.DisplayName} ({item.Name}) : " + e.ToString());
-                    }
-                }
 #if PAR
                 );
 #endif
 
-                this._logger.LogInformation("End of Sync, Sleep");
-                await Task.Delay(Interval);
+                    this._logger.LogInformation("End of Sync, Sleep");
+                    await Task.Delay(Interval);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e.ToString());
+                }
             }
         }
     }
